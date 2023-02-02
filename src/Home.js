3ef02daa833e9df7bd506631e97953fc38296421
lhachani <louayhachani@gmail.com>
2023-02-02 14:54:19 +0100

    Passing Props

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,22 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   // let name = "louay";
-  const [name, setName] = useState("louay");
-  const [age, setAge] = useState("23");
-
-  const handleClick = () => {
-    setName("hachani");
-    setAge("24");
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "React Website", body: "lorem ipsum...", author: "louay", id: 1 },
+    {
+      title: "React Website2",
+      body: "lorem ipsum...",
+      author: "hachani",
+      id: 2,
+    },
+    { title: "React Website3", body: "lorem ipsum...", author: "louay", id: 3 },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {" "}
-        {name} is {age} years old{" "}
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title="Blogs List" />
     </div>
   );
 };
